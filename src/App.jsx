e7a7fac3cac273172b79379281c49fe82e67d792
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-06 13:17:28 +0100

    feat: dinamic buttons generation completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import languages from './assets/languages'
+import { useState } from 'react'
 
 function App() {
+  const [selecedLanguage, setSelectedLanguage] = useState({
+    id: 0,
+    title: "TITOLO",
+    description: "DESCRIZIONE"
+  });
 
   return (
     <>
@@ -10,17 +16,20 @@ function App() {
         <div className="container my-5">
           <section className=''>
             {/* sezione dei bottoni */}
-            <div className='my-4'>
-              <button className="btn btn-primary">click</button>
+            <div className='my-4 d-flex gap-3'>
+              {/* <button className="btn btn-primary">click</button>
               <button className="btn">click</button>
               <button className="btn">click</button>
               <button className="btn">click</button>
-              <button className="btn">click</button>
+              <button className="btn">click</button> */}
+              {
+                languages.map((curLanguage) => <button className="btn btn-primary" key={curLanguage.id} onClick={() => console.log(curLanguage.description)}>{curLanguage.title}</button>)
+              }
             </div>
             {/* blocco di output */}
             <div className='border'>
-              <h2>Titolo</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime itaque perferendis voluptatibus aliquid deserunt modi, amet atque sint praesentium!</p>
+              <h2>{selecedLanguage.title}</h2>
+              <p>{selecedLanguage.description}</p>
             </div>
           </section>
         </div>
