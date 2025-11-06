8fa205f09e085f2ff24c8e8b471a0876edd4f284
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-06 13:08:40 +0100

    feat: base sctructure defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
-// import "bootstrap/dist/js/bootstrap.min.js"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import languages from './assets/languages'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <main>
+        <div className="container my-5">
+          <section className=''>
+            {/* sezione dei bottoni */}
+            <div className='my-4'>
+              <button className="btn btn-primary">click</button>
+              <button className="btn">click</button>
+              <button className="btn">click</button>
+              <button className="btn">click</button>
+              <button className="btn">click</button>
+            </div>
+            {/* blocco di output */}
+            <div className='border'>
+              <h2>Titolo</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maxime itaque perferendis voluptatibus aliquid deserunt modi, amet atque sint praesentium!</p>
+            </div>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
